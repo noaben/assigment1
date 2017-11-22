39e210a18433c51e2f2439e409daaaec20d20356
benhoren <33118426+benhoren@users.noreply.github.com>
2017-11-22 17:26:24 +0200

    update wifi.java
    change kml generator function

--- codes/Wifi.java
+++ codes/Wifi.java
@@ -1,12 +1,19 @@
-package munche;
-
+package lesson1;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import de.micromata.opengis.kml.v_2_2_0.Document;
+
+import de.micromata.opengis.kml.v_2_2_0.AltitudeMode;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
+import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
+
+
 /**
- * class for one wifi's point
+ * class for one wifi point
  * @author benho
  *@since 11-2017
  *
@@ -34,11 +41,12 @@ public class Wifi {
 	 * @param LON the wifi's Current Longitude
 	 * @param ALT the wifi's AltitudeMeters
 	 * @param SSID the wifi's SSID
-	 * @param MAC the wifi's BSSID address
+	 * @param MAC the wifi's BSSID adress
 	 * @param frequncy the wifi's Channel
 	 * @param signal the wifi's signal
 	 */
-	public Wifi (String time, String ID, String LAT, String LON, String ALT, String SSID, String MAC, String frequncy, String signal){
+	public Wifi (String time, String ID, String LAT, String LON, 
+			String ALT, String SSID, String MAC, String frequncy, String signal){
 		this.Time =time;
 		this.ID =ID;
 		this.LAT =LAT;
@@ -53,35 +61,25 @@ public class Wifi {
 	
 	
 	/**
-	 * check if the wifi's data is standard
+	 * check if the wifi data is standard
 	 * @param time the time
 	 * @param ID is the display
 	 * @param LAT the wifi's Current Latitude
 	 * @param LON the wifi's Current Longitude
 	 * @param ALT the wifi's AltitudeMeters
 	 * @param SSID the wifi's SSID
-	 * @param MAC the wifi's MAC address
+	 * @param MAC the wifi's MAC adress
 	 * @param frequncy the wifi's Channel
 	 * @param signal the wifi's signal
 	 * 
-	 * @return true if the data is standard false if not
+	 * @return true if ok false if abnormal
 	 */
-	public static boolean correct(String time, String ID, String LAT, String LON, String ALT, String SSID, String MAC, String frequncy, String signal){
-        if (ID.equals("") || ID.equals("null")) return false;
-        
-        if (SSID.equals("") || SSID.equals("null")) return false;
-        
-        if (MAC.equals("") || MAC.equals("null")) return false;
-        
-        try{Integer.parseInt(ALT);}
-		catch (NumberFormatException e){return false;}
-        
+	public static boolean correct(String time, String ID, String LAT, String LON, 
+			String ALT, String SSID, String MAC, String frequncy, String signal){
+
 		try{Integer.parseInt(signal);}
-		catch (NumberFormatException e){return false;} //the application has attempted to convert a string to one of the numeric types, but that the string does not have the appropriate format.
-        
-		try{Integer.parseInt(frequncy);}
-		catch (NumberFormatException e){return false;}
-		
+		catch (NumberFormatException e){return false;} 
+
 		if(signal.equals("-113")) return false;
 		
 		try{ Double.parseDouble(LAT);}
@@ -90,72 +88,71 @@ public class Wifi {
 		try{ Double.parseDouble(LON);}
 		catch (NumberFormatException e){return false;}
 		
-		if (time.substring(0,4).equals("1970")) return false;
-		
+
+		if(time.substring(0, 4).equals("1970")) return false;
+
 		return true;
 	}
 
-	public  boolean equals(Wifi other){
-		return (this.Time.equals(other.Time) && this.ID.equals(other.ID) && this.LAT.equals(other.LAT) && this.LON.equals(other.LON) && this.ALT.equals(other.ALT) && this.SSID.equals(other.SSID) && this.MAC.equals(other.MAC)&&  this.frequncy.equals(other.frequncy));
-	}
+
 	/** 
-	 * @return wifi's time
+	 * @return wifi time
 	 */
 	public String getTime() {
 		return Time;
 	}
 	/** 
-	 * @return wifi's ID
+	 * @return wifi ID
 	 */
 	public String getID() {
 		return ID;
 	}
 	/** 
-	 * @return wifi's LAT
+	 * @return wifi LAT
 	 */
 	public String getLAT() {
 		return LAT;
 	}
 	/** 
-	 * @return wifi's LON
+	 * @return wifi LON
 	 */
 	public String getLON() {
 		return LON;
 	}
 	/** 
-	 * @return wifi's ALT
+	 * @return wifi ALT
 	 */
 	public String getALT() {
 		return ALT;
 	}
 	/** 
-	 * @return wifi's SSID
+	 * @return wifi SSID
 	 */
 	public String getSSID() {
 		return SSID;
 	}
 	/** 
-	 * @return wifi's MAC
+	 * @return wifi MAC
 	 */
 	public String getMAC() {
 		return MAC;
 	}
 	/** 
-	 * @return wifi's getFrequncy
+	 * @return wifi getFrequncy
 	 */
 	public String getFrequncy() {
 		return frequncy;
 	}
 	/** 
-	 * @return wifi's signal
+	 * @return wifi signal
 	 */
 	public String getSignal() {
 		return signal;
 	}
 	
 	/**
-	 * Used to get the wifi's coordinates, split by ","
-	 * @return wifi's coordinates.
+	 * Used to get the wifi coordinates, split by ","
+	 * @return wifi coordinates.
 	 */
 	public String getCoordinates(){
 		try{ Double.parseDouble(LAT);}
@@ -168,27 +165,24 @@ public class Wifi {
 		
 	}
 	
-	public void kmlGenerator( FileWriter writer){
+	
+	public void kmlGenerator(Document doc){
 		
-		
-		
-		
-		try {
-			
-			
-			writer.append("<Placemark>");
-			writer.append('\n');
-			writer.append("<name>"+SSID+"</name>");
-			writer.append("<description><![CDATA[SSID: <b>"+SSID+"</b><br/>MAC: <b>"+MAC+"</b><br/>Alt: <b>"+ALT+"</b><br/>Channel: <b>"+frequncy+"</b><br/>Signal: <b>"+signal+"dBm</b><br/>Time: <b>"+Time+"</b>]]></description>");
-			writer.append('\n');
-			writer.append("<styleUrl>#green</styleUrl><Point> <coordinates>"+getCoordinates()+"</coordinates></Point></Placemark>");
-			writer.append('\n');
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String s="<br/><b>MAC: </b>"+MAC+"<br/><b>Alt: </b>"+ALT+"<br/><b>Channel: </b>"
+						+ ""+frequncy+"<br/><b>Signal: </b>"+signal+"dBm<br/><b>Time: </b>"
+								+ ""+Time;
+		TimeStamp ts=new TimeStamp();
+		ts.withWhen(this.Time.split(" ")[0]+'T'+this.Time.split(" ")[1]);
+		doc.createAndAddPlacemark()
+		   .withName(this.SSID)
+		   .withVisibility(true)
+		   .withOpen(true)
+		   .withDescription(s)
+		   .withStyleUrl("#green")
+		   .withTimePrimitive(ts)
+		   .createAndSetPoint()
+		   .addToCoordinates(getCoordinates());
+		 
 	}
 
 
