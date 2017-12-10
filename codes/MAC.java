b8e1e188567e89277d6dba6ef84d7b86fedac94e
NoaHadad <33871063+noaben@users.noreply.github.com>
2017-12-10 19:22:24 +0200

    Add files via upload

---
+++ codes/MAC.java
@@ -0,0 +1,64 @@
+package noa;
+
+public class MAC {
+	
+	private String ADRESS;
+	private double LAT;
+	private double LON;
+	private double ALT;
+	
+	public MAC (){
+		this.ADRESS ="0";
+		this.LAT =0;
+		this.LON =0;
+		this.ALT =0;
+	}
+
+	public MAC (String ADRESS,double LAT, double LON, double ALT){
+		this.ADRESS =ADRESS;
+		this.LAT =LAT;
+		this.LON =LON;
+		this.ALT =ALT;
+	}
+
+	
+	public boolean equals(MAC other){
+		return this.ADRESS.equals(other.ADRESS) && this.LAT==other.LAT && this.LON==other.LON && this.ALT==other.ALT;
+	}
+	public void setMAC (String ADRESS){
+	    this.ADRESS =ADRESS;
+	}
+	
+	public void setLAT (double LAT){
+	    this.LAT =LAT;
+	}
+	
+	public void setLON (double LON){
+	    this.LON =LON;
+	}
+	
+	public void setALT (double ALT){
+	    this.ALT =ALT;
+	}
+	public String getMAC (){
+	    return getMAC ();
+	}
+	
+	public double getLAT (){
+		 return this.LAT;
+	}
+	
+	public double getLON (){
+		 return this.LON;
+	}
+	
+	public double getALT (){
+		 return this.ALT;
+	}
+	
+	public String toString (){
+	   return "MAC: "+ADRESS+" LAT: "+LAT+" LON: "+LON+" ALT: "+ ALT;
+	}
+	
+
+}
---
+++ codes/Weight.java
@@ -0,0 +1,49 @@
+package noa;
+
+public class Weight {
+	private double LAT;
+	private double LON;
+	private double ALT;
+	private double W;
+  
+    Weight(double LAT,double LON,double ALT,double W){
+    	this.LAT=LAT;
+    	this.LON=LON;
+    	this.ALT=ALT;
+    	this.W=W;
+        }
+    
+    Weight(Weight other){
+    	this.LAT=other.LAT;
+    	this.LON=other.LON;
+    	this.ALT=other.ALT;
+    	this.W=other.W;
+       }
+
+   public void setWeight(Weight other){
+	   this.LAT=other.LAT;
+       this.LON=other.LON;
+       this.ALT=other.ALT;
+       this.W=other.W;
+       }
+    
+	public double getALT (){
+	    return this.ALT;
+    	}
+	
+	public double getLAT (){
+		 return this.LAT;
+	     }
+	
+	public double getLON (){
+		 return this.LON;
+	    }
+	
+	public double getW (){
+	    return this.W;
+	    }
+	
+	public String toString (){
+		   return "W: "+this.W+" LAT: "+this.LAT+" LON: "+this.LON+" ALT: "+ this.ALT;
+		}
+}
