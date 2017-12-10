ccfff45d72b5ab625825ce4f7d25c680d4756fec
NoaHadad <33871063+noaben@users.noreply.github.com>
2017-12-10 20:20:05 +0200

    Update q2Test.java

--- codes/q2Test.java
+++ codes/q2Test.java
@@ -59,6 +59,77 @@ public class q2Test {
 		    
 	
 	    assertEquals(count,739);
+		
+	    
+	    if (q2.toCSV("BM1_wifiscans","BM1.csv")==false)
+		    assertTrue(false);
+	    
+	        count=0;
+	        
+	        try {  
+		    	 
+	        
+	        FileReader fr1 = new FileReader("BM1.csv");
+			BufferedReader br1 = new BufferedReader(fr1);
+	    
+			while (br1.readLine()!=null)
+	    		count++;
+			
+	        }
+		 	catch(IOException ex) {
+				System.out.print("Error reading file \n" + ex);} 
+		    
+	
+	    assertEquals(count,348);
+	    
+	    if (q2.toCSV("BM2_wifiscans","BM2.csv")==false)
+		    assertTrue(false);
+	    
+	        count=0;
+	        
+	        try {  
+		    	 
+	        
+	        FileReader fr2 = new FileReader("BM2.csv");
+			BufferedReader br2 = new BufferedReader(fr2 );
+	    
+			while (br2.readLine()!=null)
+	    		count++;
+			
+	        }
+		 	catch(IOException ex) {
+				System.out.print("Error reading file \n" + ex);} 
+		    
+	
+	    assertEquals(count,387);
+	    
+	    if (q2.toCSV("BM3_wifiscans","BM3.csv")==false)
+		    assertTrue(false);
+	    
+	        count=0;
+	        
+	        try {  
+		    	 
+	        
+	        FileReader fr3 = new FileReader("BM3.csv");
+			BufferedReader br3 = new BufferedReader(fr3 );
+	    
+			while (br3.readLine()!=null)
+	    		count++;
+			
+	        }
+		 	catch(IOException ex) {
+				System.out.print("Error reading file \n" + ex);} 
+		    
+	
+	    assertEquals(count,107);
+	
+	
+	
+	
+	   }
+  }
+
 	
 	   }
   }
