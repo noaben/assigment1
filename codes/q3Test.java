3a0122fb9dbe81e90d8b7edd5de1d9c0e108545a
NoaHadad <33871063+noaben@users.noreply.github.com>
2017-12-15 23:43:34 +0200

    Update q3Test.java

--- codes/q3Test.java
+++ codes/q3Test.java
@@ -48,12 +48,23 @@ public class q3Test {
 	
 	@Test
 	public void test3() {
-                MAC mac1=new MAC("88:dc:96:17:c0:9e",32.1623059,34.8085222,27.2660323);
-		assertTrue("The required MAC is the same that we excepted ",q3.findPlaceAlgorithm1("wifiTester.csv","88:dc:96:17:c0:9e").equals(mac1));
-		MAC mac2=new MAC("14:ae:db:3d:b1:55",32.1671457,34.8080293,39.9270507);
-		assertTrue("The required MAC is the same that we excepted ",q3.findPlaceAlgorithm1("wifiTester.csv","14:ae:db:3d:b1:55").equals(mac2));
-		MAC mac3=new MAC("e0:10:7f:49:37:fc",32.1617363,34.8091938,19.8406876);
-		assertTrue("The required MAC is the same that we excepted ",q3.findPlaceAlgorithm1("wifiTester.csv","e0:10:7f:49:37:fc").equals(mac3));
+		
+	    MAC mac1=q3.findPlaceAlgorithm1("wifiTester.csv","88:dc:96:17:c0:9e");
+	    assertTrue((32.1623059-((int)(mac1.getLAT()*10000000))/10000000.0000000)==0);
+	    assertTrue((34.8085222-((int)(mac1.getLON()*10000000))/10000000.0000000)==0);
+            assertTrue((27.2660323-((int)(mac1.getALT()*10000000))/10000000.0000000)==0);
+
+            MAC mac2=q3.findPlaceAlgorithm1("wifiTester.csv","14:ae:db:3d:b1:55");
+            assertTrue((32.1671457-((int)(mac2.getLAT()*10000000))/10000000.0000000)==0);
+	    assertTrue((34.8080293-((int)(mac2.getLON()*10000000))/10000000.0000000)==0);
+            assertTrue((39.9270507-((int)(mac2.getALT()*10000000))/10000000.000000)==0);
+        
+	    MAC mac3=q3.findPlaceAlgorithm1("wifiTester.csv","e0:10:7f:49:37:fc");
+	    assertTrue((32.1617363-((int)(mac3.getLAT()*10000000))/10000000.0000000)==0);
+	    assertTrue((34.8091938-((int)(mac3.getLON()*10000000))/10000000.0000000)==0);
+            assertTrue((19.8406876-((int)(mac3.getALT()*10000000))/10000000.0000000)==0);
+
+
 	}
 	
 
@@ -64,30 +75,30 @@ public class q3Test {
 	@Test
 	public void test4() {
 		
-       q3.findPlaceAlgorithm2("wifiTester.csv", "testFile_q3_algo2.csv","testResult_q3_algo2.csv");
+        q3.findPlaceAlgorithm2("wifiTester.csv", "testFile_q3_algo2.csv","testResult_q3_algo2.csv");
 		
         try{
-                    FileReader fr = new FileReader("testResult_q3_algo2.csv");
+            FileReader fr = new FileReader("testResult_q3_algo2.csv");
 		    BufferedReader br = new BufferedReader(fr );
         
 			
 		    String str1=br.readLine();
 		    String arr1[]=str1.split(",");
-		    assertEquals("32.168722",arr1[2]);
-		    assertEquals("34.8130424",arr1[3]);
-	            assertEquals("36.7719216",arr1[4]);
+		    assertEquals("32.168722",arr1[2].substring(0,9));
+		    assertEquals("34.8130424",arr1[3].substring(0,10));
+	            assertEquals("36.771921",arr1[4].substring(0,9));
+	   
 		
 		    String str2=br.readLine();
 		    String arr2[]=str2.split(",");
-		    assertEquals("32.1680669",arr2[2]);
-		    assertEquals("34.8101319",arr2[3]);
-		    assertEquals("27.1057282",arr2[4]);	
-		    
-                   }
+		    assertEquals("32.1680669",arr2[2].substring(0,10));
+		    assertEquals("34.8101319",arr2[3].substring(0,10));
+		    assertEquals("27.105728",arr2[4].substring(0,9));	
+           }
 		
 	   catch(IOException ex) {
-		    System.out.print("Error reading file \n" + ex);} 
-	}
+		   System.out.print("Error reading file \n" + ex);} 
+	       }
 	
 }
 	
