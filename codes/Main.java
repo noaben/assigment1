ec89667f0ff1586448510973fe681daefec54c3d
NoaHadad <33871063+noaben@users.noreply.github.com>
2018-01-03 02:38:53 +0200

    Update Main.java

--- codes/Main.java
+++ codes/Main.java
@@ -1,6 +1,9 @@
 package matala001;
 
 
+import java.awt.EventQueue;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
@@ -8,16 +11,120 @@ public class Main {
 	
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		
+		LinkedList <Wifi> data_base=new LinkedList <Wifi> ();
+		LinkedList <Wifi> data_not_filtered=new LinkedList <Wifi> ();
+		ArrayList <File> folder=new ArrayList <File>();
+		ArrayList <Long> folder_last_modified=new ArrayList <Long>();
+		ArrayList <File> combs=new ArrayList <File>();
+		ArrayList <Long> combs_last_modified=new ArrayList <Long>();
+		String s[]=new String [9];
+		s[0]="0";//number of items that been filtered
+		
+		
+		int t=1;
+		
+		
+		frame1 window = new frame1(data_base,data_not_filtered,folder,folder_last_modified,combs,combs_last_modified,s);
+		window.frame.setVisible(true);
+		
+		Thread a=new Thread(new Runnable() {
+				  public void run() {
+					  while (t==1){
+						
+						try {
+							
+							System.out.println(data_base.size());
+							boolean flag=false;
+							int i=0;
+							if (!flag && i<folder.size()){
+						     	for (;i<folder.size();i++){
+								    if (folder.get(i).lastModified()!=folder_last_modified.get(i)){
+								    	System.out.println(folder.get(i).lastModified()+" "+folder_last_modified.get(i));
+									    flag=true;
+								       }
+						      	   }
+							   }
+						
+							i=0;
+							if (!flag && i<combs.size()){
+						     	for (;i<combs.size();i++){
+								    if (combs.get(i).lastModified()!=combs_last_modified.get(i)){
+									    flag=true;
+					     	            }
+						            }
+							   }
+							System.out.println(flag);
+							if (flag){
+							
+								i=0;
+								data_base.clear();
+								folder_last_modified.clear();
+								combs_last_modified.clear();
+								System.out.println(data_base.size());
+								for (;i<folder.size();i++){
+									folder_last_modified.add(folder.get(i).lastModified());
+									data_base.addAll(combiningData.list(folder.get(i).getName()));
+								   }
+								i=0;
+								for (;i<combs.size();i++){
+									combs_last_modified.add(combs.get(i).lastModified());
+									data_base.addAll(combiningData.list(combs.get(i).getName()));
+								}
+							  
+							if (s[0].equals("1") ){
+								combiningData.listToCsv(data_base, "c.csv");
+								data_base.clear();
+								data_base.addAll(processingData.list("c.csv", "filtering", s[2], s[3], s[1]));
+							  }
+							if (s[0].equals("2") ){
+								combiningData.listToCsv(data_base, "c.csv");
+								data_base.clear();
+								data_base.addAll(processingData.list("c.csv", "filtering", s[2], s[3], s[1],s[5],s[6],s[4],s[7],s[8]));
+							   }
+							
+							
+						  }	int m=(int)Math.random()*15000;
+							Thread.sleep(m);}
+						
+						catch (Exception e) {
+							e.printStackTrace();
+					}
+					   System.out.println(s[0]);
+					  }	  }});
+		   
+	   /* Thread b=new Thread(new Runnable() {
+		  public void run() {
+		
+				try {
+					frame1 window = new frame1(data_base,data_not_filtered,folder,folder_last_modified,combs,combs_last_modified);
+					window.frame.setVisible(true);
+					
+					Thread.sleep(i);
+					 
+				}
+				
+				catch (Exception e) {
+					e.printStackTrace();
+			
+			    }	  }});*/
+	   
+	
+	   a.start();
+	//   b.start();
+	}
+
+		
 		 
-		String folder="wifiFolder";
+	//	String folder="wifiFolder";
 		String CSVpath="wificsv.csv";
 		String weightedMac="weightedMac.csv";
 		
-		LinkedList <Wifi> data_base=combiningData.list(folder);
+	//	LinkedList <Wifi> data_base=combiningData.list(folder);
 
-	        combiningData.toCSV(folder, CSVpath);
+	//    combiningData.toCSV(folder, CSVpath);
 		
 		String KMLpath = "kmlFile.kml";
 		
@@ -34,15 +141,14 @@ public class Main {
        //	String requiredData = "2017-11-01 14:24";
 		
 		
-               processingData.CSVtoKML(CSVpath, KMLpath, filter, filterBy, requiredData,5, weightedMac,"yes");
+            //   processingData.CSVtoKML(CSVpath, KMLpath, filter, filterBy, requiredData,5, weightedMac,"yes");
 		
-      	       processingData.listOrganized( "_comb_all_BM2_.csv",processingData.list("_comb_all_BM2_.csv",filter, filterBy, requiredData,"yes"),4,"Algo1_4_BM2_comb_all_.csv");
-      	       processingData.listOrganized( "_comb_all_BM3_.csv",processingData.list("_comb_all_BM3_.csv",filter, filterBy, requiredData,"yes"),4,"Algo1_4_BM3_comb_all_.csv");
-               algorithms.findPlaceAlgorithm2( "_comb_all_BM3_.csv", "_comb_no_gps_ts1.csv",4,10000,0.4,2,"Algo2_BM3_TS1_.csv");
-               algorithms.findPlaceAlgorithm2( "_comb_all_BM3_.csv", "_comb_no_gps_ts2_.csv",4,10000,0.4,2,"Algo2_BM3_TS2.csv");
-               algorithms.findPlaceAlgorithm2( "_comb_all_BM2_.csv", "_comb_no_gps_ts1.csv",4,10000,0.4,2,"Algo2_BM2_TS1.csv");
-	       algorithms.findPlaceAlgorithm2( "_comb_all_BM2_.csv", "_comb_no_gps_ts2_.csv",4,10000,0.4,2,"Algo2_BM2_TS2.csv");
+      	    //   processingData.listOrganized( "_comb_all_BM2_.csv",processingData.list("_comb_all_BM2_.csv",filter, filterBy, requiredData,"yes"),4,"Algo1_4_BM2_comb_all_.csv");
+      	    //   processingData.listOrganized( "_comb_all_BM3_.csv",processingData.list("_comb_all_BM3_.csv",filter, filterBy, requiredData,"yes"),4,"Algo1_4_BM3_comb_all_.csv");
+            //   algorithms.findPlaceAlgorithm2( "_comb_all_BM3_.csv", "_comb_no_gps_ts1.csv",4,10000,0.4,2,"Algo2_BM3_TS1_.csv");
+            //   algorithms.findPlaceAlgorithm2( "_comb_all_BM3_.csv", "_comb_no_gps_ts2_.csv",4,10000,0.4,2,"Algo2_BM3_TS2.csv");
+           //    algorithms.findPlaceAlgorithm2( "_comb_all_BM2_.csv", "_comb_no_gps_ts1.csv",4,10000,0.4,2,"Algo2_BM2_TS1.csv");
+	       //algorithms.findPlaceAlgorithm2( "_comb_all_BM2_.csv", "_comb_no_gps_ts2_.csv",4,10000,0.4,2,"Algo2_BM2_TS2.csv");
 		
 	}
-}
 
