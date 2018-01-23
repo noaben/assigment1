129e3da1baf205ed578fa316e24e1b2e1777eb52
NoaHadad <33871063+noaben@users.noreply.github.com>
2018-01-23 21:59:34 +0200

    Update connectsql.java

--- codes/connectsql.java
+++ codes/connectsql.java
@@ -37,52 +37,53 @@ public class connectSQL {
      
 public LinkedList<Wifi> getData() throws SQLException{
 	Statement st = null;
-    ResultSet rs = null;
-	  Connection _con = null;
-	  LinkedList<Wifi> list=new  LinkedList<Wifi> ();
-	 try{
-	 _con = DriverManager.getConnection(url, user, password);
-     st = _con.createStatement();
-     String query="SELECT * FROM "+tavla;
-     rs = st.executeQuery(query);
+        ResultSet rs = null;
+	Connection _con = null;
+	LinkedList<Wifi> list=new  LinkedList<Wifi> ();
+	try{
+	   _con = DriverManager.getConnection(url, user, password);
+           st = _con.createStatement();
+           String query="SELECT * FROM "+tavla;
+           rs = st.executeQuery(query);
     
-	 while (rs.next()){
+	   while (rs.next()){
 		 int wifis_number=Integer.parseInt(rs.getString(7));
 		 for (int i=0;i<wifis_number;i++){
-		 Wifi w=new Wifi(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),null,rs.getString(8+i*2),null,rs.getString(9+i*2));
-		 list.add(w);
-		 }
-	 }
-     }
-	 catch (SQLException el){
-		 System.out.println("no connection");
-	 }
-	 return list;
+		      Wifi w=new Wifi(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),null,rs.getString(8+i*2),null,rs.getString(9+i*2));
+		      list.add(w);
+		      }
+	         }
+          }
+       catch (SQLException el){
+          System.out.println("no connection");
+	  }
+       return list;
 
-}
+       }
+	
 public String lastModified() throws SQLException{
 	try{
 	  Connection _con = null;
 	  _con = DriverManager.getConnection(url,user,password);
 	  Statement st = null;
 	  ResultSet rs = null;
-      st = _con.createStatement();
-      rs = st.executeQuery("SELECT UPDATE_TIME FROM information_schema.tables WHERE TABLE_SCHEMA = 'oop_course_ariel' AND TABLE_NAME = 'ex4_db'");
-      if (rs.next())      System.out.println(rs.getString(1));
-      return rs.getString(1);
-	}
-	 catch (SQLException el){
-		 System.out.println("no connection");
+          st = _con.createStatement();
+          rs = st.executeQuery("SELECT UPDATE_TIME FROM information_schema.tables WHERE TABLE_SCHEMA = 'oop_course_ariel' AND TABLE_NAME = 'ex4_db'");
+          if (rs.next())      System.out.println(rs.getString(1));
+          return rs.getString(1);
+	 }
+       catch (SQLException el){
+         System.out.println("no connection");
 	 }
 	 return null;
 }
 
 public static void main(String[] args) throws SQLException {
 	connectSQL a=new connectSQL("5.29.193.52","oop_course_ariel","oop1","Lambda1();","ex4_db","mysql","3306");
-    LinkedList <Wifi>w1=a.getData();
-    System.out.println(w1.size());
-    a.lastModified();
-}
+        LinkedList <Wifi>w1=a.getData();
+        System.out.println(w1.size());
+        a.lastModified();
+      }
 }
 
    
